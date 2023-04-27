031d46136e4a98c3326d33cd02a39559bb8d3917
Jasmin Paige <jaspagmaz98@gmail.com>
2023-04-27 15:43:20 +0800

    Created a dictionary for management moving forward

--- gwa_reader.py
+++ gwa_reader.py
@@ -3,21 +3,24 @@
 
 # Open the text file containing the list of students and their gwa's
 
-import sys
-print (sys.path)
-
 with open("C:/Users/HomePC/OOP_SourceCode/GWAReader/class_list.txt") as student_file:
     file_reader = student_file.readlines()
-    print (file_reader)
+    class_record_dict = {}
+       
+    # Separate the student's name from their attained marks
     
-# Determine the highest grade that a student can recieve
+    for line in file_reader:
+        student_name, student_gwa = line.strip().split(",")
+        class_record_dict[student_name] = student_gwa
+        print (class_record_dict)
+    
+    # Determine the highest and lowest grade that a student can recieve
+    
+    
+    
+    # Have the program read each gwa and compare it with the highest grade level a student can recieve.   
+    
 
-highest_grade = 1.00
-
-# Determine the lowest grade that a student can recieve
-
-lowest_grade = 5.00
-
-# Separate the student's name from their attained marks
-    # Have the program read each gwa and compare it with the highest grade level a student can recieve.
-    # # Print the name and the grade of the student with the highest grade and highest GWA.
+         # Print the name and the grade of the student with the highest grade and highest GWA.
+         # Hint: The highest grade presented should be Joshua Adrian Cabanatuan - 1.53
+        
