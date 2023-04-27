dcb819421045ae09441c927523a2457289f42446
Jasmin Paige <jaspagmaz98@gmail.com>
2023-04-27 11:24:15 +0800

    Program guide has been made, proceed to coding

---
+++ gwa_reader.py
@@ -0,0 +1,11 @@
+# Write a Python program that read a file containing the name of 20 students together with their GWA. 
+# The program will outputs the name of the student who got the highest GWA (including the GWA).
+
+# Open the text file containing the list of students and their gwa's
+# Determine the highest grade that a student can recieve
+# Separate the student's name from their attained marks
+# Have the program read each gwa and compare it with the highest grade level a student can recieve.
+# Print the name and the grade of the student with the highest grade and highest GWA.
+
+
+
