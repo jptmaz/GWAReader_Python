9954e1d02483fa33df649d94826f7e898bded5ba
Jasmin Paige <jaspagmaz98@gmail.com>
2023-04-27 12:06:17 +0800

    Program can now read the text file

--- gwa_reader.py
+++ gwa_reader.py
@@ -2,10 +2,16 @@
 # The program will outputs the name of the student who got the highest GWA (including the GWA).
 
 # Open the text file containing the list of students and their gwa's
+
+import sys
+print (sys.path)
+
+with open("C:/Users/HomePC/OOP_SourceCode/GWAReader/class_list.txt") as student_file:
+    file_reader = student_file.readlines()
+    print (file_reader)
+    
 # Determine the highest grade that a student can recieve
+# Determine the lowest grade that a student can recieve
 # Separate the student's name from their attained marks
-# Have the program read each gwa and compare it with the highest grade level a student can recieve.
-# Print the name and the grade of the student with the highest grade and highest GWA.
-
-
-
+    # Have the program read each gwa and compare it with the highest grade level a student can recieve.
+    # # Print the name and the grade of the student with the highest grade and highest GWA.
